61c78b895c27150c7bdac31f7bbb2b19bf32fe24
andy <project.command.center@gmail.com>
2025-07-13 12:20:01 -0400

    .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,33 +8,30 @@ module.exports = {
       jsx: true
     }
   },
-  plugins: ["@typescript-eslint", "react", "react-hooks", "unused-imports"],
+  plugins: ["@typescript-eslint", "react", "react-hooks"],
   extends: [
     "next",
     "next/core-web-vitals",
     "eslint:recommended",
-    "plugin:react/recommended",
     "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
     "plugin:react-hooks/recommended",
     "plugin:prettier/recommended"
   ],
   rules: {
+    // отключаем любые ошибки за неиспользуемые переменные
+    "@typescript-eslint/no-unused-vars": "off",
+    "no-unused-vars": "off",
+
+    // отключаем авто-ошибки unused-imports (если ты ставил этот плагин ранее)
+    "unused-imports/no-unused-vars": "off",
+    "unused-imports/no-unused-imports": "off",
+
     "@typescript-eslint/no-explicit-any": "off",
-    "@typescript-eslint/no-unused-vars": "warn",
     "@typescript-eslint/ban-ts-comment": "off",
     "@typescript-eslint/no-require-imports": "off",
     "no-case-declarations": "warn",
-    "no-console": "off",
-    "unused-imports/no-unused-imports": "warn",
-    "unused-imports/no-unused-vars": [
-      "warn",
-      {
-        vars: "all",
-        varsIgnorePattern: "^_",
-        args: "after-used",
-        argsIgnorePattern: "^_"
-      }
-    ]
+    "no-console": "off"
   },
   settings: {
     react: {
