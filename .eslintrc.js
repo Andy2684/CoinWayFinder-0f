33615e2da13c78a0e13cd2877c7a83abe8aa946c
andy <project.command.center@gmail.com>
2025-07-13 12:15:10 -0400

    .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,47 +1,18 @@
-module.exports = {
-  root: true,
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
-    ecmaFeatures: {
-      jsx: true
+rules: {
+  "@typescript-eslint/no-explicit-any": "off",
+  "@typescript-eslint/no-unused-vars": "warn",
+  "@typescript-eslint/ban-ts-comment": "off",
+  "@typescript-eslint/no-require-imports": "off",
+  "no-case-declarations": "warn",
+  "no-console": "off",
+  "unused-imports/no-unused-imports": "warn",
+  "unused-imports/no-unused-vars": [
+    "warn",
+    {
+      vars: "all",
+      varsIgnorePattern: "^_",
+      args: "after-used",
+      argsIgnorePattern: "^_"
     }
-  },
-  plugins: ["@typescript-eslint", "react", "react-hooks", "unused-imports"],
-  extends: [
-    "next",
-    "next/core-web-vitals",
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react-hooks/recommended",
-    "plugin:prettier/recommended"
-  ],
-  rules: {
-    // отключаем ошибки по any
-    "@typescript-eslint/no-explicit-any": "off",
-    // предупреждение вместо ошибки
-    "@typescript-eslint/no-unused-vars": "warn",
-    "no-case-declarations": "warn",
-    "no-console": "off",
-    "@typescript-eslint/ban-ts-comment": "off",
-
-    // неиспользуемые импорты — удалить
-    "unused-imports/no-unused-imports": "warn",
-    "unused-imports/no-unused-vars": [
-      "warn",
-      {
-        vars: "all",
-        varsIgnorePattern: "^_",
-        args: "after-used",
-        argsIgnorePattern: "^_"
-      }
-    ]
-  },
-  settings: {
-    react: {
-      version: "detect"
-    }
-  }
+  ]
 }
