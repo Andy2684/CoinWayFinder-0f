9ba66257e4b6b99f7303c09db1856362fa915b45
andy <project.command.center@gmail.com>
2025-07-13 12:17:51 -0400

    .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,44 @@
-rules: {
-  "@typescript-eslint/no-explicit-any": "off",
-  "@typescript-eslint/no-unused-vars": "warn",
-  "@typescript-eslint/ban-ts-comment": "off",
-  "@typescript-eslint/no-require-imports": "off",
-  "no-case-declarations": "warn",
-  "no-console": "off",
-  "unused-imports/no-unused-imports": "warn",
-  "unused-imports/no-unused-vars": [
-    "warn",
-    {
-      vars: "all",
-      varsIgnorePattern: "^_",
-      args: "after-used",
-      argsIgnorePattern: "^_"
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true
     }
-  ]
+  },
+  plugins: ["@typescript-eslint", "react", "react-hooks", "unused-imports"],
+  extends: [
+    "next",
+    "next/core-web-vitals",
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:prettier/recommended"
+  ],
+  rules: {
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-unused-vars": "warn",
+    "@typescript-eslint/ban-ts-comment": "off",
+    "@typescript-eslint/no-require-imports": "off",
+    "no-case-declarations": "warn",
+    "no-console": "off",
+    "unused-imports/no-unused-imports": "warn",
+    "unused-imports/no-unused-vars": [
+      "warn",
+      {
+        vars: "all",
+        varsIgnorePattern: "^_",
+        args: "after-used",
+        argsIgnorePattern: "^_"
+      }
+    ]
+  },
+  settings: {
+    react: {
+      version: "detect"
+    }
+  }
 }
